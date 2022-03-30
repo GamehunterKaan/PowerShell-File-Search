73ddd1147775b44be12c00bc1e56cc0686867a2f
GamehunterKaan <68380968+GamehunterKaan@users.noreply.github.com>
2022-03-30 22:01:21 +0300

    Add files via upload

---
+++ file_search.ps1
@@ -0,0 +1,13 @@
+function search {
+  param($search)
+  $drives = $drives = (get-volume | where-Object {$_.DriveLetter -like "?"}).DriveLetter
+  foreach ($drive in $drives) {
+    $files += Get-ChildItem -Recurse -Path "$drive`:\" -Filter "*.txt" -ErrorAction SilentlyContinue | Select-String "$search" -List -ErrorAction SilentlyContinue | Select Path  
+  }
+
+  foreach ($file in $files.Path) {
+    if ("$file" -like "*txt*") {
+        Write-Host "[+] $file"
+    }
+  }
+}
