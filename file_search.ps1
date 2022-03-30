eed308a1feba1020517fcdfaeb770ee412b041d1
GamehunterKaan <68380968+GamehunterKaan@users.noreply.github.com>
2022-03-30 22:06:38 +0300

    Update file_search.ps1

--- file_search.ps1
+++ file_search.ps1
@@ -2,6 +2,7 @@ function search {
   param($search)
   $drives = $drives = (get-volume | where-Object {$_.DriveLetter -like "?"}).DriveLetter
   foreach ($drive in $drives) {
+    Write-Host "[*]Searching in $drive`:\"
     $files += Get-ChildItem -Recurse -Path "$drive`:\" -Filter "*.txt" -ErrorAction SilentlyContinue | Select-String "$search" -List -ErrorAction SilentlyContinue | Select Path  
   }
 
